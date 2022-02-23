3ff06119374b45aa8e468517789aa5b893a7497f
BBIBBI1i <eir0118@naver.com>
2022-02-23 17:26:38 +0900

    20220223_Last

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{  
+    // 이ㅗㄷㅇ에 사용할 리지드바디 컴포넌트
+    public Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    // 이동속력
+    public float speed = 8f;
+
+    void Start()
+    {
+       
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
